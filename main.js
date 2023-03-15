f7512cc8d7d4cd9a0e018583dc77ec0e1b9378f1
edoardorizzo <edo96.rizzo@gmail.com>
2023-03-15 14:58:22 +0100

    print array element

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 /* Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while. */
- let shopping = [
+let shopping = [
     'Banane',
     'Pasta',
     'Tonno',
     'Ragu',
     'Pane'
- ]
+]
+
+ for (let i = 0; i < shopping.length; i++) {
+    const shoppingList = shopping[i];
+    console.log(shoppingList);
+
+    const listElm = document.querySelector('.list_item');
+    listElm.innerHTML += (shopping);
+    break
+ }
